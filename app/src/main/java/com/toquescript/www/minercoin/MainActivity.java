9c77611a381f2bae521008570ce11fddefc6289a
admin <luiguibalarezo@gmail.com>
2017-01-05 18:30:59 -0500

    Dia 2 Zeta

--- app/src/main/java/com/toquescript/www/minercoin/MainActivity.java
+++ app/src/main/java/com/toquescript/www/minercoin/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -14,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
